b015ad8043b84ee872dc916c0fa11e8d46a6a478
Simon Hughesdon <simon@hughesdon.com>
2014-11-06 11:09:52 +0000

    Tweak paving script

--- DevMachineInstall/devApps.ps1
+++ DevMachineInstall/devApps.ps1
@@ -1,32 +1,33 @@
 # Dev apps to install
 
-# Visual studio stuff
-cinst VisualStudio2012Ultimate
-
-# Visual studio add-ins
-cinst xUnit.vs2012
-cinst WebEssentials2012
-#cinst resharper -Version 7.1.3000.2254 # Use only resharper if you want the latests. This is what I have a license for
-cinst typescript
-cinst typescript.vs
-cinst SpecFlow
-
-# databases and tools
-cinst MsSqlServer2012Express
-#cinst mongodb
-#cinst MongoVUE
-
 # browsers
 cinst Firefox
 cinst GoogleChrome
 
 # Other tools
-cinst git
+cinst githubforwindows
+cinst poshgit
+
 cinst notepadplusplus
+cinst sublimetext2
+cinst markdownpad2
 cinst winmerge
-cinst Console2
+# cinst Console2
 cinst 7zip
-cinst nodejs.install
 cinst fiddler
 cinst curl
-cinst linqpad4
+
+# Node
+cinst nodejs.install
+
+# Ruby
+cinst ruby
+cinst ruby2.devkit
+
+# VS
+#cinst VisualStudio2013Ultimate
+#cinst nunit
+
+# Sync
+cinst dropbox
+cinst 1password
